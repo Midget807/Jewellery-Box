0857130043389b3ac74b85e121ea476eeb3b1f3a
Midget807 <phu7mai@gmail.com>
2025-01-05 23:27:47 +1030

    render finished

--- src/main/java/net/midget807/jewellery_box/client/renderer/JewelleryBoxEntityRenderer.java
+++ src/main/java/net/midget807/jewellery_box/client/renderer/JewelleryBoxEntityRenderer.java
@@ -23,13 +23,12 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.RotationAxis;
 import net.minecraft.world.LightType;
 import net.minecraft.world.World;
-import org.joml.Quaternionf;
 
 public class JewelleryBoxEntityRenderer implements BlockEntityRenderer<JewelleryBoxBlockEntity> {
     public static final Identifier FULL_TEXTURE = JewelleryBoxMain.id("textures/entity/jewellery_box/full.png");
     public static final Identifier HALF_TEXTURE = JewelleryBoxMain.id("textures/entity/jewellery_box/half.png");
     public static final Identifier QUARTER_TEXTURE = JewelleryBoxMain.id("textures/entity/jewellery_box/quarter.png");
-    public static final float SCALE_MAGNITUDE = 0.35f;
+    public static final float SCALE_MAGNITUDE = 0.25f;
     private static final String BASE = "base";
     private static final String LID = "lid";
     public final ModelPart fullBase;
@@ -137,24 +136,93 @@ public class JewelleryBoxEntityRenderer implements BlockEntityRenderer<Jewellery
         BlockState blockState = worldNull ? entity.getCachedState() : ModBlocks.FULL_JEWELLERY_BOX.getDefaultState().with(JewelleryBoxBlock.FACING, Direction.SOUTH);
         float rotation = ((Direction) blockState.get(JewelleryBoxBlock.FACING)).asRotation();
 
+
         //Item Renderer
         matrices.push();
-        matrices.translate(0.5f, 0.75f, 0.5f);
+        matrices.translate(0.5f, 0.32f, 0.5f);
         matrices.scale(SCALE_MAGNITUDE, SCALE_MAGNITUDE, SCALE_MAGNITUDE);
-        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(-rotation));
-        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(270));
-        if (entity.size != 0 || !entity.inventory.isEmpty()) {
-            itemRenderer.renderItem(
-                    stacks.get(0),
-                    ModelTransformationMode.GUI,
-                    getLightLevel(entity.getWorld(), entity.getPos()),
-                    OverlayTexture.DEFAULT_UV,
-                    matrices,
-                    vertexConsumers,
-                    entity.getWorld(),
-                    1
-            );
+        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(rotation));
+        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(90));
+        /*
+        switch (entity.size) {
+            case 2 -> this.renderItems(Math.min(stacks.size(), 2), entity, stacks, itemRenderer, getLightLevel(entity.getWorld(), entity.getPos()), matrices, vertexConsumers, world, (int) entity.getPos().asLong());
+            case 4 -> this.renderItems(Math.min(stacks.size(), 2), entity, stacks, itemRenderer, getLightLevel(entity.getWorld(), entity.getPos()), matrices, vertexConsumers, world, (int) entity.getPos().asLong());
+            default -> this.renderItems(Math.min(stacks.size(), 2), entity, stacks, itemRenderer, getLightLevel(entity.getWorld(), entity.getPos()), matrices, vertexConsumers, world, (int) entity.getPos().asLong());
         }
+        */
+        DefaultedList<ItemStack> availableRenderStacks = this.getRenderStacks(entity, stacks);
+        if (!availableRenderStacks.isEmpty()) {
+            switch (availableRenderStacks.size()) {
+                case 4:
+                    this.renderEvenItems(
+                            false,
+                            2,
+                            2,
+                            availableRenderStacks.get(0),
+                            availableRenderStacks.get(1),
+                            availableRenderStacks.get(2),
+                            availableRenderStacks.get(3),
+                            matrices,
+                            itemRenderer,
+                            getLightLevel(entity.getWorld(), entity.getPos()),
+                            vertexConsumers,
+                            world,
+                            (int) entity.getPos().asLong()
+                    );
+                    break;
+                case 3:
+                    this.renderOddItems(
+                            false,
+                            2,
+                            2,
+                            1,
+                            availableRenderStacks.get(0),
+                            availableRenderStacks.get(1),
+                            availableRenderStacks.get(2),
+                            matrices,
+                            itemRenderer,
+                            getLightLevel(entity.getWorld(), entity.getPos()),
+                            vertexConsumers,
+                            world,
+                            (int) entity.getPos().asLong()
+                    );
+                    break;
+                case 2:
+                    this.renderEvenItems(
+                            entity.size <= 4,
+                            2,
+                            1,
+                            availableRenderStacks.get(0),
+                            availableRenderStacks.get(1),
+                            ItemStack.EMPTY,
+                            ItemStack.EMPTY,
+                            matrices,
+                            itemRenderer,
+                            getLightLevel(entity.getWorld(), entity.getPos()),
+                            vertexConsumers,
+                            world,
+                            (int) entity.getPos().asLong()
+                    );
+                    break;
+                default:
+                    this.renderOddItems(
+                            entity.size <= 2,
+                            1,
+                            1,
+                            0,
+                            availableRenderStacks.get(0),
+                            ItemStack.EMPTY,
+                            ItemStack.EMPTY,
+                            matrices,
+                            itemRenderer,
+                            getLightLevel(entity.getWorld(), entity.getPos()),
+                            vertexConsumers,
+                            world,
+                            (int) entity.getPos().asLong()
+                    );
+            }
+        }
+
         matrices.pop();
 
         //Block Entity Renderer
@@ -191,6 +259,193 @@ public class JewelleryBoxEntityRenderer implements BlockEntityRenderer<Jewellery
         lid.pitch = -(openFactor * (float) (Math.PI / 2));
         lid.render(matrices, vertices, light, overlay);
         base.render(matrices, vertices, light, overlay);
+    }
+    private DefaultedList<ItemStack> getRenderStacks(
+            JewelleryBoxBlockEntity entity,
+            DefaultedList<ItemStack> inventory
+    ) {
+
+        ItemStack stack1 = ItemStack.EMPTY;
+        ItemStack stack2 = ItemStack.EMPTY;
+        ItemStack stack3 = ItemStack.EMPTY;
+        ItemStack stack4 = ItemStack.EMPTY;
+        assert entity.size != 0 || !entity.inventory.isEmpty();
+        for (int k1 = 0; k1 < inventory.size(); k1++) {
+            if (inventory.get(k1) != ItemStack.EMPTY && inventory.size() >= 2) {
+                for (int k2 = k1 + 1; k2 < inventory.size(); k2++) {
+                    if (inventory.get(k2) != ItemStack.EMPTY && inventory.size() >= 4) {
+                        for (int k3 = k2 + 1; k3 < inventory.size(); k3++) {
+                            if (inventory.get(k3) != ItemStack.EMPTY && inventory.size() >= 8) {
+                                for (int k4 = k3 + 1; k4 < inventory.size(); k4++) {
+                                    if (inventory.get(k4) != ItemStack.EMPTY) {
+                                        stack4 = inventory.get(k4);
+                                        break;
+                                    }
+                                }
+                                stack3 = inventory.get(k3);
+                                break;
+                            }
+                        }
+                        stack2 = inventory.get(k2);
+                        break;
+                    }
+                }
+                stack1 = inventory.get(k1);
+                break;
+            }
+        }
+        DefaultedList<ItemStack> availableStacks = DefaultedList.of();
+        if (stack1 == ItemStack.EMPTY) {
+            return availableStacks;
+        } else {
+            availableStacks.add(0, stack1);
+            if (stack2 == ItemStack.EMPTY) {
+                return availableStacks;
+            } else {
+                availableStacks.add(1, stack2);
+                if (stack3 == ItemStack.EMPTY) {
+                    return availableStacks;
+                } else {
+                    availableStacks.add(2, stack3);
+                    if (stack4 == ItemStack.EMPTY) {
+                        return availableStacks;
+                    } else {
+                        availableStacks.add(3, stack4);
+                    }
+                }
+            }
+        }
+        return availableStacks;
+    }
+
+    private void renderEvenItems(
+            boolean halfSize,
+            int maxColumnSize,
+            int maxRowSize,
+            ItemStack stack1,
+            ItemStack stack2,
+            ItemStack stack3,
+            ItemStack stack4,
+            MatrixStack matrices,
+            ItemRenderer itemRenderer,
+            int light,
+            VertexConsumerProvider vertexConsumers,
+            World world,
+            int seed
+    ) {
+        ItemStack renderedStack;
+        for (int i = 1; i <= maxRowSize; i++) {
+            for (int j = 1; j <= maxColumnSize; j++) {
+                if (j == 2) {
+                    if (i == 2) {
+                        renderedStack = stack4;
+                    } else {
+                        renderedStack = stack2;
+                    }
+                } else {
+                    if (i == 2) {
+                        renderedStack = stack3;
+                    } else {
+                        renderedStack = stack1;
+                    }
+                }
+                if (renderedStack != ItemStack.EMPTY) {
+                    matrices.push();
+                    matrices.translate(1.0 - (1.8 * (j)), 1.0 - (1.8 * (i)), 0.0f);
+                    if (j == 2) {
+                        matrices.translate(0.5f, 0.0f, 0.0f);
+                    }
+                    if (i == 2) {
+                        matrices.translate(0.0f, 0.5f, 0.0f);
+                    }
+                    if (maxRowSize == 2) {
+                        matrices.translate(1.45f, 1.45f, 0.0f);
+                    } else {
+                        matrices.translate(1.45f, 0.8f, 0.0f);
+                    }
+                    if (halfSize) {
+                        matrices.translate(0.0f, 0.0f, 0.35f);
+                    }
+                    itemRenderer.renderItem(
+                            renderedStack,
+                            ModelTransformationMode.FIXED,
+                            light,
+                            OverlayTexture.DEFAULT_UV,
+                            matrices,
+                            vertexConsumers,
+                            world,
+                            seed + j
+                    );
+                    matrices.pop();
+                }
+            }
+        }
+    }
+
+    private void renderOddItems(
+            boolean quarterSize,
+            int maxColumnSizeR1,
+            int maxRowSize,
+            int maxColumnSizeR2,
+            ItemStack stack1,
+            ItemStack stack2,
+            ItemStack stack3,
+            MatrixStack matrices,
+            ItemRenderer itemRenderer,
+            int light,
+            VertexConsumerProvider vertexConsumers,
+            World world,
+            int seed
+    ) {
+        ItemStack renderedStack;
+        for (int i = 1; i <= maxRowSize; i++) {
+            for (int j = 1; j <= (i == 1 ? maxColumnSizeR1 : maxColumnSizeR2); j++) {
+                if (j == 2) {
+                    if (i == 2) {
+                        renderedStack = stack3;
+                    } else {
+                        renderedStack = stack2;
+                    }
+                } else {
+                        renderedStack = stack1;
+                }
+                if (renderedStack != ItemStack.EMPTY) {
+                    matrices.push();
+                    matrices.translate(1.0 - (1.8 * (j)), 1.0 - (1.8 * (i)), 0.0f);
+                    if (i == 2 && j == 1) {
+                        matrices.translate(1.0 - 1.65, 0.0f, 0.0f);
+                    }
+                    if (j == 2) {
+                        matrices.translate(0.5f, 0.0f, 0.0f);
+                    }
+                    if (i == 2) {
+                        matrices.translate(0.0f, 0.5f, 0.0f);
+                    }
+                    if (maxRowSize == 2) {
+                        matrices.translate(1.45f, 1.45f, 0.0f);
+                    } else {
+                        matrices.translate(0.675f, 0.675f, 0.0f);
+                    }
+                    if (i == 1 && j == 1 && stack2 == ItemStack.EMPTY) {
+                        matrices.translate(0.125f, 0.125f, 0.0f);
+                        if (quarterSize) {
+                            matrices.translate(0.0f, 0.0f, 0.5f);
+                        }
+                    }
+                    itemRenderer.renderItem(
+                            renderedStack,
+                            ModelTransformationMode.FIXED,
+                            light,
+                            OverlayTexture.DEFAULT_UV,
+                            matrices,
+                            vertexConsumers,
+                            world,
+                            seed + j
+                    );
+                    matrices.pop();
+                }
+            }
+        }
     }
 
     private int getLightLevel(World world, BlockPos pos) {
